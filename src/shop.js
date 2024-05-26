155687013ac6d694a1d9f20e67043560dbd874f6
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-05-26 16:09:56 +0200

    initial

--- src/shop.js
+++ src/shop.js
@@ -3,43 +3,42 @@ import './shop.css';
 
 const Shop = ({ coins, coinPerClick, upgradeCost, upgradeLevel, onClose, onUpgrade, onUpgradeEnergy, clickLimit, upgradeLevelEnergy, upgradeCostEnergy }) => {
   return (
-    <div class="shop">
-      <h2>Магазин</h2>
-      <div id="CoinPerTap">
-        <p>Монет за клик </p>
-        <div id="BuyCoinPerTapMenu">
-          <p id="MainInfo">{coinPerClick}</p>
-          <div id="CoinPerTapInfo">
-            <p>Уровень улучшения: {upgradeLevel}</p>
+      <div className="shop">
+        <h2>Магазин</h2>
+        <div id="CoinPerTap">
+          <p>Монет за клик</p>
+          <div id="BuyCoinPerTapMenu">
+            <p id="MainInfo">{coinPerClick}</p>
+            <div id="CoinPerTapInfo">
+              <p>Уровень улучшения: {upgradeLevel}</p>
+            </div>
+            <div id="CoinPerTapInfo">
+              <p>Стоимость улучшения: {upgradeCost}</p>
+            </div>
+            <button onClick={onUpgrade} disabled={coins < upgradeCost}>
+              Улучшить
+            </button>
           </div>
-          <div id="CoinPerTapInfo">
-            <p>Стоимость улучшения: {upgradeCost}</p>
-          </div>
-          <button onClick={onUpgrade} disabled={coins < upgradeCost}>
-            Улучшить
-          </button>
         </div>
-      </div>
 
-      <div id="CoinPerTap">
-        <p>Енергия</p>
-        <div id="BuyCoinPerTapMenu">
-          <p id="MainInfo">{clickLimit}</p>
-          <div id="CoinPerTapInfo">
-            <p>Уровень улучшения: {upgradeLevelEnergy}</p>
+        <div id="CoinPerTap">
+          <p>Энергия</p>
+          <div id="BuyCoinPerTapMenu">
+            <p id="MainInfo">{clickLimit}</p>
+            <div id="CoinPerTapInfo">
+              <p>Уровень улучшения: {upgradeLevelEnergy}</p>
+            </div>
+            <div id="CoinPerTapInfo">
+              <p>Стоимость улучшения: {upgradeCostEnergy}</p>
+            </div>
+            <button onClick={onUpgradeEnergy} disabled={coins < upgradeCostEnergy}>
+              Улучшить
+            </button>
           </div>
-          <div id="CoinPerTapInfo">
-            <p>Стоимость улучшения: {upgradeCostEnergy}</p>
-          </div>
-          <button onClick={onUpgradeEnergy} disabled={coins < upgradeCostEnergy}>
-            Улучшить
-          </button>
         </div>
+
+        <button onClick={onClose} id="clouseButtom">Закрыть</button>
       </div>
-
-      <button onClick={onClose} id="clouseButtom">Закрыть</button>
-
-    </div>
   );
 };
 
