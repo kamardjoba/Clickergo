7413eefb3a66fa9a24c46b2e96f7fded009140c2
Aferist163 <aferist.game.163@gmail.com>
2024-05-26 06:57:53 +0200

    initial

--- src/shop.js
+++ src/shop.js
@@ -8,7 +8,23 @@ const Shop = ({ coins, coinPerClick, upgradeCost, upgradeLevel, onClose, onUpgra
       <div id="CoinPerTap">
         <p>Монет за клик </p>
         <div id="BuyCoinPerTapMenu">
-          <p>{coinPerClick}</p>
+          <p id="MainInfo">{coinPerClick}</p>
+          <div id="CoinPerTapInfo">
+            <p>Уровень улучшения: {upgradeLevel}</p>
+          </div>
+          <div id="CoinPerTapInfo">
+            <p>Стоимость улучшения: {upgradeCost}</p>
+          </div>
+          <button onClick={onUpgrade} disabled={coins < upgradeCost}>
+            Улучшить
+          </button>
+        </div>
+      </div>
+
+      <div id="CoinPerTap">
+        <p>Монет за клик </p>
+        <div id="BuyCoinPerTapMenu">
+          <p id="MainInfo">{coinPerClick}</p>
           <div id="CoinPerTapInfo">
             <p>ровень улучшения: {upgradeLevel}</p>
           </div>
@@ -19,9 +35,10 @@ const Shop = ({ coins, coinPerClick, upgradeCost, upgradeLevel, onClose, onUpgra
             Улучшить
           </button>
         </div>
-        
       </div>
+
       <button onClick={onClose}>Закрыть</button>
+
     </div>
   );
 };
