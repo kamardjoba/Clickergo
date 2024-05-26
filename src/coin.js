e130b4c990e60485174566dc5b9163d74e8925e2
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-05-26 19:59:14 +0200

    initial

--- src/coin.js
+++ src/coin.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import  {motion}  from 'framer-motion';
+import { motion } from 'framer-motion';
 import coinImage from './coin.png';
 import './coin.css';
 
 const Coin = ({ onClick }) => {
-  return (
-    <motion.div
-      className="coin"
-      onClick={onClick}
-      whileTap={{ rotate: [0, 360] }}
-      transition={{ duration: 0.5 }}
-    >
-      <img src={coinImage} alt="Coin" />
-    </motion.div>
 
+  const handleMouseDown = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left; // X-coordinate relative to the coin
+    const y = event.clientY - rect.top;  // Y-coordinate relative to the coin
+    const rotateX = ((y / rect.height) - 0.5) * -20; // Rotate in Y axis
+    const rotateY = ((x / rect.width) - 0.5) * 20;  // Rotate in X axis
+
+    event.target.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  };
+
+  const handleMouseUp = (event) => {
+    event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  };
+
+  return (
+      <motion.div
+          className="coin"
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onClick={onClick}
+      >
+        <img src={coinImage} alt="Coin" />
+      </motion.div>
   );
 };
 
