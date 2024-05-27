e1853ee7f9b3c523c3497f3aab8ef6564859b6b4
Aferist163 <aferist.game.163@gmail.com>
2024-05-27 17:41:43 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
 
   return (
     <div class="App">
       <div class = "info">
-        <img src="N.png" alt="Icon">
+        <img src="N.png" alt="Icon"></img>
         <p> Name </p>
-        <img src="b.png" alt="Bifclif">
+        <img src="b.png" alt="Bifclif"></img>
       </div>
       <div class = "main">
         <div class="mainInfo">
           <div class="halfBox">
             <div class = "halfBoxDiv">
               <p> Coin Tap</p>
-              <p>+1 <img src="CU.png" alt="Coin" class="coin-image"></p>
+              <p>+1 <img src="CU.png" alt="Coin" class="coin-image"></img></p>
             </div>
           </div>
           <div class="halfBox">
             <div class = "halfBoxDiv">
               <p> Coin per hour</p>
-              <p>+0 <img src="CU.png" alt="Coin" class="coin-image"></p>
+              <p>+0 <img src="CU.png" alt="Coin" class="coin-image"></img></p>
             </div>
           </div>
         </div>
       
       <div class="CoinInfo">			
-        <img src="CU.png" alt="Coin">
+        <img src="CU.png" alt="Coin"></img> 
         <p> 1000</p>			
       </div>
-      <img src="C.png" alt="Coin">
+      <img src="C.png" alt="Coin"></img> 
       <div class = "lower">
         <div class = "lowerDiv">
-          <img src="b.png" alt="Bifclif">
+          <img src="b.png" alt="Bifclif"></img>
           <p>Shop</p>
           <p>🔋</p>
           <p>🚀</p>
