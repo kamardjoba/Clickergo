59873dd439a24f034a9d03e90db0e3e4b057d62f
Aferist163 <aferist.game.163@gmail.com>
2024-05-27 17:38:02 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ function App() {
   return (
     <div class="App">
       <div class = "info">
-        <img src="N.png" alt="Icon"></img>
+        <img src="N.png" alt="Icon">
         <p> Name </p>
-        <img src="b.png" alt="Bifclif"></img>
+        <img src="b.png" alt="Bifclif">
       </div>
       <div class = "main">
         <div class="mainInfo">
           <div class="halfBox">
             <div class = "halfBoxDiv">
               <p> Coin Tap</p>
-              <p>+1 <img src="CU.png" alt="Coin" class="coin-image"></img></p>
+              <p>+1 <img src="CU.png" alt="Coin" class="coin-image"></p>
             </div>
           </div>
           <div class="halfBox">
             <div class = "halfBoxDiv">
               <p> Coin per hour</p>
-              <p>+0 <img src="CU.png" alt="Coin" class="coin-image"></img></p>
+              <p>+0 <img src="CU.png" alt="Coin" class="coin-image"></p>
             </div>
           </div>
         </div>
       
       <div class="CoinInfo">			
-        <img src="CU.png" alt="Coin"></img> 
+        <img src="CU.png" alt="Coin">
         <p> 1000</p>			
       </div>
-      <img src="C.png" alt="Coin"></img> 
+      <img src="C.png" alt="Coin">
       <div class = "lower">
         <div class = "lowerDiv">
-          <img src="b.png" alt="Bifclif"></img>
+          <img src="b.png" alt="Bifclif">
           <p>Shop</p>
           <p>🔋</p>
           <p>🚀</p>
