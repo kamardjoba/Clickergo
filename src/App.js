36cff285ca09c02b75b19e84657e3a0f6add5f5a
Aferist163 <aferist.game.163@gmail.com>
2024-05-27 17:57:22 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import coinImage from './C.png';
 
 function App() {
 
   return (
     <div class="App">
       <div class = "info">
-        <img src="C:\Users\struk\Desktop\Main Game\GitHub\Clickergo\src\N.png" alt="Icon"></img>
+        <img src="N.png" alt="Icon"/>
         <p> Name </p>
-        <img src="b.png" alt="Bifclif"></img>
+        <img src="b.png" alt="Bifclif"/>
       </div>
       <div class = "main">
         <div class="mainInfo">
           <div class="halfBox">
             <div class = "halfBoxDiv">
               <p> Coin Tap</p>
-              <p>+1 <img src="CU.png" alt="Coin" class="coin-image"></img></p>
+              <p>+1 <img src="CU.png" alt="Coin" class="coin-image"/></p>
             </div>
           </div>
           <div class="halfBox">
             <div class = "halfBoxDiv">
               <p> Coin per hour</p>
-              <p>+0 <img src="CU.png" alt="Coin" class="coin-image"></img></p>
+              <p>+0 <img src="CU.png" alt="Coin" class="coin-image"/></p>
             </div>
           </div>
         </div>
       
       <div class="CoinInfo">			
-        <img src="CU.png" alt="Coin"></img> 
+        <img src="CU.png" alt="Coin"/>
         <p> 1000</p>			
       </div>
-      <img src="./C.png" alt="Coin"></img> 
+      <img src={coinImage} alt="Coin"/>
       <div class = "lower">
         <div class = "lowerDiv">
-          <img src="b.png" alt="Bifclif"></img>
+          <img src="b.png" alt="Bifclif"/>
           <p>Shop</p>
           <p>🔋</p>
           <p>🚀</p>
