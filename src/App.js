e86d9fb1b81cb7f95845732170a18580eb3424ab
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-05-27 22:57:23 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import coinImage from './C.png';
 import coinIcon from './CU.png';
 import Icon from './N.png';
 import logo from './b.png';
 
-
 function App() {
-
   return (
-  <body>
-    <div class="App">
-      <div class = "info">
-        <img src={Icon} alt="Icon"/>
-        <p> Name </p>
-        <img src={logo} alt="Bifclif"/>
+    <div className="App">
+      <div className="info">
+        <img src={Icon} alt="Icon" />
+        <p>Name</p>
+        <img src={logo} alt="Bifclif" />
       </div>
-      <div class = "main">
-        <div class="mainInfo">
-          <div class="halfBox">
-            <div class = "halfBoxDiv">
-              <p> Coin Tap</p>
-              <p>+1 <img src={coinIcon} alt="Coin" class="coin-image"/></p>
-            </div>
+      <div className="main">
+        <div className="mainInfo">
+          <div className="halfBox">
+            <p>Coin Tap</p>
+            <p>+1 <img src={coinIcon} alt="Coin" className="coin-image" /></p>
           </div>
-          <div class="halfBox">
-            <div class = "halfBoxDiv">
-              <p> Coin per hour</p>
-              <p>+0 <img src={coinIcon} alt="Coin" class="coin-image"/></p>
-            </div>
+          <div className="halfBox">
+            <p>Coin per hour</p>
+            <p>+0 <img src={coinIcon} alt="Coin" className="coin-image" /></p>
           </div>
         </div>
-        <div class="CoinInfo">			
-          <img src={coinIcon} alt="Coin" height = "90%" />
-          <p> 1000</p>			
+        <div className="CoinInfo">
+          <img src={coinIcon} alt="Coin" height="90%" />
+          <p>1000</p>
         </div>
-        <img src={coinImage} alt="Coin" height = "50%"/>
-        <div class = "lower">
-          <div class = "lowerDiv">
-            <img src={logo} alt="Bifclif"/>
-            <p>Shop</p>
-            <p>🔋</p>
-            <p>🚀</p>
-          </div>
+        <img src={coinImage} alt="Coin" height="50%" />
+        <div className="lower">
+          <img src={logo} alt="Bifclif" />
+          <p>Shop</p>
+          <p>🔋</p>
+          <p>🚀</p>
         </div>
       </div>
     </div>
-  </body>
   );
 }
 
