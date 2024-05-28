5c9ecee3a352f1407ef3dcdb99511a832bb7e246
Aferist163 <aferist.game.163@gmail.com>
2024-05-28 07:03:08 +0200

    initial

--- src/Coin.js
+++ src/Coin.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import  {motion}  from 'framer-motion';
 import coinImage from './C.png';
+import './Coin.css';
 
 const Coin = ({ onClick }) => {
   return (
-    <motion.div className="coin" onClick={onClick}>
+    <motion.div class="coin" onClick={onClick}>
       <img src={coinImage} alt="Coin" height="50%"/>
     </motion.div>
 
