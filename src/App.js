43f10e97b8fc44189d841faeeecfdab0837fe713
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-05-29 02:17:37 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
-=======
-import React, { useState } from 'react';
->>>>>>> 7f320ad55cc722d58b3bc60eabc9f6fd8d3f0aa3
 import './App.css';
 import coinIcon from './CU.png';
 import Icon from './N.png';
@@ -10,23 +6,15 @@ import logo from './b.png';
 import coinImage from './C.png';
 import BB from './BB.png';
 import ProgressBar from './ProgressBar';
-<<<<<<< HEAD
 import axios from 'axios';
 
 function App() {
   const [userId, setUserId] = useState(null);
-=======
-
-
-function App() {
-
->>>>>>> 7f320ad55cc722d58b3bc60eabc9f6fd8d3f0aa3
   const [coins, setCoins] = useState(0);
   const [clicks, setClicks] = useState(0);
   const clickLimit = 1000;
   const coinPerClick = 1;
 
-<<<<<<< HEAD
   useEffect(() => {
     const telegram = window.Telegram.WebApp;
     const telegramId = telegram.initDataUnsafe.user.id;
@@ -59,8 +47,6 @@ function App() {
     }
   }, [coins, clicks, userId]);
 
-=======
->>>>>>> 7f320ad55cc722d58b3bc60eabc9f6fd8d3f0aa3
   const handleCoinClick = () => {
     if (clicks < clickLimit) {
       setCoins(coins + coinPerClick);
@@ -69,7 +55,6 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
       <div className="App">
         <div className="info">
           <img src={Icon} alt="Icon"/>
@@ -109,49 +94,6 @@ function App() {
           </div>
         </div>
       </div>
-=======
-  <body>
-    <div class="App">
-      <div class = "info">
-        <img src={Icon} alt="Icon"/>
-        <p> Name </p>
-        <img src={logo} alt="Bifclif"/>
-      </div>
-      <div class = "main">
-        <div class="mainInfo">
-          <div class="halfBox">
-            <div class = "halfBoxDiv">
-              <p> Coin Per Tap</p>
-              <p>+{coinPerClick} <img src={coinIcon} alt="Coin" class="coin-image"/></p>
-            </div>
-          </div>
-          <div class="halfBox">
-            <div class = "halfBoxDiv">
-              <p> Energy </p>
-              <p>{clickLimit} / {clickLimit-clicks}<img src={BB} alt="Battery" class="coin-image"/></p>
-            </div>
-          </div>
-        </div>
-        <div class="CoinInfo">			
-          <img src={coinIcon} alt="Coin" height = "90%" />
-          <p>{coins}</p>			
-        </div>
-          <img src={coinImage} onClick={handleCoinClick} alt="Coin" height="50%"/>
-        <div class="Progress">
-        <ProgressBar current={clicks} max={clickLimit} />
-		    </div>
-        <div class = "lower">
-          <div class = "lowerDiv">
-            <img src={logo} alt="Bifclif"/>
-            <p>Shop</p>
-            <p>🔋</p>
-            <p>🚀</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
->>>>>>> 7f320ad55cc722d58b3bc60eabc9f6fd8d3f0aa3
   );
 }
 
