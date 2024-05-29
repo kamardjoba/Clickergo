d117a1f9366b4a8836764dc284bb32f68ac3a4e7
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2024-05-29 02:20:49 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -55,37 +55,38 @@ function App() {
   };
 
   return (
-      <div className="App">
-        <div className="info">
+      <body>
+      <div class="App">
+        <div class = "info">
           <img src={Icon} alt="Icon"/>
           <p> Name </p>
           <img src={logo} alt="Bifclif"/>
         </div>
-        <div className="main">
-          <div className="mainInfo">
-            <div className="halfBox">
-              <div className="halfBoxDiv">
+        <div class = "main">
+          <div class="mainInfo">
+            <div class="halfBox">
+              <div class = "halfBoxDiv">
                 <p> Coin Per Tap</p>
-                <p>+{coinPerClick} <img src={coinIcon} alt="Coin" className="coin-image"/></p>
+                <p>+{coinPerClick} <img src={coinIcon} alt="Coin" class="coin-image"/></p>
               </div>
             </div>
-            <div className="halfBox">
-              <div className="halfBoxDiv">
+            <div class="halfBox">
+              <div class = "halfBoxDiv">
                 <p> Energy </p>
-                <p>{clickLimit} / {clickLimit-clicks}<img src={BB} alt="Battery" className="coin-image"/></p>
+                <p>{clickLimit} / {clickLimit-clicks}<img src={BB} alt="Battery" class="coin-image"/></p>
               </div>
             </div>
           </div>
-          <div className="CoinInfo">
-            <img src={coinIcon} alt="Coin" height="90%" />
+          <div class="CoinInfo">
+            <img src={coinIcon} alt="Coin" height = "90%" />
             <p>{coins}</p>
           </div>
           <img src={coinImage} onClick={handleCoinClick} alt="Coin" height="50%"/>
-          <div className="Progress">
+          <div class="Progress">
             <ProgressBar current={clicks} max={clickLimit} />
           </div>
-          <div className="lower">
-            <div className="lowerDiv">
+          <div class = "lower">
+            <div class = "lowerDiv">
               <img src={logo} alt="Bifclif"/>
               <p>Shop</p>
               <p>🔋</p>
@@ -94,7 +95,9 @@ function App() {
           </div>
         </div>
       </div>
+      </body>
   );
+
 }
 
 export default App;
