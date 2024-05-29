c3b0f199798b3bd7a790f8aa78e513756955ad64
Aferist163 <aferist.game.163@gmail.com>
2024-05-30 01:51:10 +0200

    initial

--- src/coin.js
+++ src/coin.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import coinImage from './C.png';
 import './coin.css';
 
 const Coindiv = ({ onClick }) => {
+
+    const handleInteractionStart = (event) => {
+        const touchEvent = event.type === 'touchstart' ? event.touches[0] : event;
+        const rect = event.target.getBoundingClientRect();
+        const x = touchEvent.clientX - rect.left;
+        const y = touchEvent.clientY - rect.top;
+        const rotateX = ((y / rect.height) - 0.5) * -20;
+        const rotateY = ((x / rect.width) - 0.5) * 20;
+    
+        event.target.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      };
+    
+      const handleInteractionEnd = (event) => {
+        event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
+      };
+
   return (
-    <div className='Podsos'>
-      <img src={coinImage} alt="Coin" height="90%" onClick={onClick}/>
-    </div>
+    <motion.div className='Podsos'
+                onMouseDown={handleInteractionStart}
+                onMouseUp={handleInteractionEnd}>
+                    
+      <img src={coinImage} alt="Coin" height="100%" onClick={onClick}/>
+    </motion.div>
   );
 };
 
