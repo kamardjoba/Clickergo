de7cab9a7fe9fb91f70111a2e297df8c7a49c161
Aferist163 <aferist.game.163@gmail.com>
2024-05-30 00:43:14 +0200

    initial

--- src/coin.js
+++ src/coin.js
@@ -1,6 +1,11 @@
 import coinImage from './C.png';
 import './coin.css';
 
-<div className='Podsos'>
+const Coindiv = () => {
+
+return(
+<div className='Podsos' >
     <img src={coinImage} alt="Coin" height="50%"/>
 </div>
+)
+};
