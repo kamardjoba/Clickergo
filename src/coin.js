aab48484faa16f3ab662e7c015e2bc9e7ebc9b8e
Aferist163 <aferist.game.163@gmail.com>
2024-05-30 04:59:40 +0200

    initial

--- src/coin.js
+++ src/coin.js
@@ -3,7 +3,8 @@ import { motion, AnimatePresence } from 'framer-motion';
 import coinImage from './C.png';
 import './coin.css';
 
-const Coindiv = ({ onClick}) => {
+const Coindiv = ({ onClick, coinPerClick, energyNow}) => {
+  const [clicksArray, setClicksArray] = useState([]);
 
   const handleInteractionStart = (event) => {
     const touchEvent = event.type === 'touchstart' ? event.touches[0] : event;
@@ -20,6 +21,21 @@ const Coindiv = ({ onClick}) => {
     event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
   };
 
+  const NumberUpAnim = (event) => {
+    const touchEvent = event.type === 'touchstart' ? event.touches[0] : event;
+    if (coinPerClick <=  energyNow) return;
+    
+    const rect = event.target.getBoundingClientRect();
+    const x = touchEvent.clientX - rect.left;
+    const y = touchEvent.clientY - rect.top;
+    
+    setClicksArray((prevClicks) => [
+    ...prevClicks,
+    { id: Date.now(), x, y, value: coinPerClick },
+    ]);
+    onClick();
+  };
+
   return (
     <motion.div className='Podsos'>
 
@@ -28,8 +44,26 @@ const Coindiv = ({ onClick}) => {
             height="105%" 
             onMouseDown={handleInteractionStart}
             onMouseUp={handleInteractionEnd}
-            onClick={onClick}/>
+            onClick={NumberUpAnim}
+            onTouchStart={(event) => { handleInteractionStart(event); handleCoinClick(event); }}
+            onTouchEnd={handleInteractionEnd}
+            />
 
+      <AnimatePresence>
+        {clicksArray.map((click) => (
+            <motion.div
+              key={click.id}
+              className="click-value"
+              initial={{ opacity: 1, y: 0 }}
+              animate={{ opacity: 0, y: -50 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 2 }}
+              style={{ top: click.y, left: click.x }}
+            >
+              +{click.value}
+            </motion.div>
+        ))}
+      </AnimatePresence>
     </motion.div>
   );
 };
